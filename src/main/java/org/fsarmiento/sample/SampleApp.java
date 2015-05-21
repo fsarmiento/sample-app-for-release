26a512e8ddeee82baa8647ce70f0660a805b98bf
Florencio Sarmiento <florenciosarmiento@Florencios-MacBook-Air.local>
2015-05-22 00:26:23 +0100

    Initial commit

---
+++ src/main/java/org/fsarmiento/sample/SampleApp.java
@@ -0,0 +1,14 @@
+package org.fsarmiento.sample;
+
+public class SampleApp {
+	
+	public static void main(String[] args) {
+		SampleApp sample = new SampleApp();
+		sample.sayHello();
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/org/fsarmiento/sample/SampleAppTest.java
@@ -0,0 +1,13 @@
+package org.fsarmiento.sample;
+
+import org.junit.Test;
+
+public class SampleAppTest {
+
+	private SampleApp classUnderTest = new SampleApp();
+	
+	@Test
+	public void test() {
+		classUnderTest.sayHello();
+	}
+}
